e8564aa4fa1d63776cd5352966dc0c099573ce1f
Thomas Almeida <99335340+tgo-mas@users.noreply.github.com>
2023-03-02 15:45:59 -0300

    Adicionando exemplos com objetos JS

---
+++ js/aula0203/exemplos.js
@@ -0,0 +1,17 @@
+
+// Aula 10: Aninhação e array de objetos
+
+let pessoa = {
+    nome: "Thomas Almeida",
+    cpf: "109.164.555-43",
+    email: 'talmeida@gmail.com',
+    endereco: {
+        cidade: "Santana do Seridó",
+        estado: "RN",
+        cep: "59350-000",
+        rua: "Nilton Ginae",
+        numero: 45
+    }
+}
+
+console.log(pessoa)
