edcb129d5c644ef8d4a556078ff560db875fc1b3
tgo-mas <talmeidasf@gmail.com>
2023-03-21 11:16:25 -0300

    Commit inicial

---
+++ aulas-react/new-estudos-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Home } from './components/Home/Home';
+import { Menu } from "./components/Menu/Menu"
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ aulas-react/new-estudos-react/src/components/Menu/Menu.jsx
@@ -0,0 +1,9 @@
+export function Menu(){
+    return(
+        <div>
+            <a href="#">Home</a>
+            <a href="#">Blog</a>
+            <a href="#">Contato</a>
+        </div>
+    );
+}
